323099096073e26fde28e754508f07efd9d3c355
Your Name <you@example.com>
2024-01-22 16:01:03 +0100

    MAJ avant le tasks

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
 
   if (projectsState.selectedProjectId === null) {
@@ -79,17 +79,16 @@ function App() {
   );
 
 
- function handleDeleteProject (){
-  console.log("ondelete")
-  setProjectsState((prevState) =>
-   {
-    return {
-      ...prevState,
-      selectedProjectId: undefined,
-      projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId
-      ),
-    };
-  });
-}
+  function handleDeleteProject() {
+    console.log("ondelete")
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
 }
 export default App;
